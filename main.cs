f36862d0bbabac9ef595bff1e310933fa7d0ab16
MT-Aiden <99355180+MT-Aiden@users.noreply.github.com>
2022-04-01 04:11:33 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,21 @@
+// Created by: Aiden McLeod
+// Created on: Sep 2022
+//
+// This program finds area of a kite
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter the Diagonal A:");
+        double Diagonala = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter the Diagonal B:");
+        double Diagonalb = Convert.ToDouble(Console.ReadLine());
+        
+        double Area = (Diagonala * Diagonalb) / 2;
+        Console.Write("Area of a Kite = " + Area + "cm²");
+        Console.ReadKey();
+    }
+}
